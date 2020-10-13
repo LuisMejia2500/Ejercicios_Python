e60f3032beac124a6fd1706325f7407af444b206
Luis Gustavo Mejía Barrios <71474291+LuisMejia2500@users.noreply.github.com>
2020-10-12 23:20:04 -0500

    Add files via upload

---
+++ MAP.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+lista=[20,18,8,4,10,40,6]
+lista2=list(map(lambda numero: int(numero/2),lista))
+
+
+# In[5]:
+
+
+lista2
+
+
+# In[ ]:
+
+
+
+
