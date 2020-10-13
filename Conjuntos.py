fe592e11e57e94d34c4620c3bf7b0db3d239d617
Luis Gustavo Mejía Barrios <71474291+LuisMejia2500@users.noreply.github.com>
2020-10-12 23:15:27 -0500

    Add files via upload

---
+++ Conjuntos.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+Lista=["manzana","platano","melocoton","pera","manzana", "pera","Limon","platano","naranja"]
+conjunto=set (Lista)
+ListaF= list(conjunto)
+print(ListaF)
+
+
+# In[ ]:
+
+
+
+
