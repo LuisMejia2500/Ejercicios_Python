69039d68a8e0e309529854705c68b09af5f17a14
Luis Gustavo Mejía Barrios <71474291+LuisMejia2500@users.noreply.github.com>
2020-10-12 23:31:07 -0500

    Add files via upload

---
+++ arrays.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+lista=[1,2,3,4,5]
+
+
+# In[4]:
+
+
+array = np.array(lista)
+
+
+# In[5]:
+
+
+array
+
+
+# In[11]:
+
+
+lista2=[[1,2,3,4],[5,6,7,8],[9,8,7,4]]
+
+
+# In[12]:
+
+
+array2 = np.array(lista2)
+
+
+# In[13]:
+
+
+array2
+
+
+# In[ ]:
+
+
+
+
