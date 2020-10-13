d44f493fb4fe092177e2b2ad21af2b67356c273d
Luis Gustavo Mejía Barrios <71474291+LuisMejia2500@users.noreply.github.com>
2020-10-12 22:54:41 -0500

    Add files via upload

---
+++ Contadordepalabras.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Contador de palabras
+# 
+
+# In[19]:
+
+
+Frase=(input())
+Cantidad=0
+
+
+# In[20]:
+
+
+Lista = Frase.split()
+
+
+# In[21]:
+
+
+Lista
+
+
+# In[22]:
+
+
+for elementos in Lista:
+    Cantidad +=1
+    
+
+
+# In[23]:
+
+
+print (Cantidad)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
