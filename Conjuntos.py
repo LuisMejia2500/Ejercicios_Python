2fae33d6fe74576cb70dbcab29863f7a786f3973
LuisMejia2500 <luis.mejia-b@uniminuto.edu.co>
2020-10-19 20:36:21 -0500

    1.0.0

--- Conjuntos.py
+++ Conjuntos.py
@@ -4,7 +4,7 @@
 # In[4]:
 
 
-Lista=["manzana","platano","melocoton","pera","manzana", "pera","Limon","platano","naranja"]
+Lista=["manzana","platano","melocoton","pera","manzana", "pera","Limon","platano","naranja","Peras"]
 conjunto=set (Lista)
 ListaF= list(conjunto)
 print(ListaF)
